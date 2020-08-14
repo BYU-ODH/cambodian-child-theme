a59193e0dc37e768bdec44d1f4c3bf7fb066b6c0
WizzyMiller <caleb.th6@gmail.com>
2020-08-14 15:02:06 -0600

    Update functions.php
    Remove Search Pane

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@
 		if (is_page ('280')) { 
 		  ?>  
 		<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-		<script src="https://cdn.datatables.net/plug-ins/preview/searchPane/dataTables.searchPane.min.js"></script>
+		<!--<script src="https://cdn.datatables.net/plug-ins/preview/searchPane/dataTables.searchPane.min.js"></script>-->
 			  <script type="text/javascript">
 				
 				jQuery(document).ready(function() {
