12cc12b0af4eb6e42d1a185e89b7315bbcd99a4c
Spencer Cheng <spencercheng2000@gmail.com>
2023-04-03 10:38:43 -0600

    Data from Interview pods

--- single-interviewee.php
+++ single-interviewee.php
@@ -1,25 +1,28 @@
 <?php
 get_header();
 
-$obj = pods();
+$obj = pods(); // Pods for Interviewee
 
 $birth_year = $obj -> field("birth_year");
 $birth_month = $obj -> field("birth_month");
 $birth_date = $obj -> field("birth_date");
-
-
 $birth_location_province = $obj -> field("birth_location_province");
 $birth_location_village = $obj -> field("birth_location_village");
 $participated_in_interview = $obj -> field("participated_in_interview");
-// var_dump($participated_in_interview);
 $picture = $obj -> field("picture");
 
- // We need to get this number dynamically
+ // Interview Pods Relationship
 $interview_pod = pods('interview', $participated_in_interview[0]["ID"]);
 $interview_month = $interview_pod -> field("interview_month");
 $interview_year = $interview_pod -> field("interview_year");
 $interview_day = $interview_pod -> field("interview_day");
+$interviewer = $interview_pod -> field("interviewer"); // echo($interviewer["post_title"]) Have to grab post_title
+$link_to_box_folder = $interview_pod -> field("link_to_box_folder");
+$video_link = $interview_pod -> field("video_link");
+$audio_link = $interview_pod -> field("audio_link");
+$transcript_file = $interview_pod -> field("transcript_file");
+$translation_file = $interview_pod -> field("translation_file");
+$story_included = $interview_pod -> field("story_included"); // $story_included[0]["post_title"] name $story_included[0]['guid'] permalink;
 
-echo($interview_month.$interview_day.$interview_year);
 ?>
 <?php get_footer();?>
