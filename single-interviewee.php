a56ff43f93b6abf62c0c9767aa65204abd8c04f4
Spencer Cheng <spencercheng2000@gmail.com>
2023-04-05 09:52:55 -0600

    pods to Php

--- single-interviewee.php
+++ single-interviewee.php
@@ -27,50 +27,60 @@ $story_included = $interview_pod -> field("story_included"); // $story_included[
 
 <div class="individual-interviewee">
     <div class="interviewee-text">
-        <?php if ($birth_year): ?>
-            <?php if ($birth_month): ?>
-                <?php if ($birth_date): ?>
-                    <p><span class="bold-heading">Birthday:</span> <?php echo $birth_date . ' ' . $birth_month . ' ' . $birth_year; ?></p>
-                <?php else: ?>
-                    <p><span class="bold-heading">Birth month:</span> <?php echo $birth_month . ' ' . $birth_year; ?></p>
-                <?php endif; ?>
-            <?php else: ?>
-                <p><span class="bold-heading">Birth year:</span> <?php echo $birth_year; ?></p>
-            <?php endif; ?>
-        <?php endif; ?>
+        <?php if ($birth_year && $birth_month && $birth_date) {
+            echo("<span class='bold-heading'>Birthday:</span>".$birth_date . ' ' . $birth_month . ' ' . $birth_year);
+            } 
+            else if ($birth_month && $birth_year) {
+                echo("<p><span class='bold-heading'>Birth month:</span>".$birth_month . ' ' . $birth_year."</p>");
+            }
+            else {
+                echo("<p><span class='bold-heading'>Birth year:</span>".$birth_year."</p>");
+            }
+        ?>
+        <!-- Birth Location -->
+        <?php if ($birth_location_province) {
+            echo("<p><span class='bold-heading'>Birth province:</span>");
+            echo("<a href='$birth_location_province[guid]'>".$birth_location_province['post_title']."</a></p>");
+        } ?>
+        <!-- Birth location Village -->
+        <?php if ($birth_location_village) {
+            echo("<p><span class='bold-heading'>Birth village:</span>");
+            echo("<a href='$birth_location_village[0][guid]'>".$birth_location_village['post_title']."</a></p>");
+        }?>
 
-        <?php if ($birth_location_province): ?>
-            <p><span class="bold-heading">Birth province:</span> <a href="<?php echo $birth_location_province[0]['guid']; ?>"><?php echo $birth_location_province['post_title']; ?></a></p>
-        <?php endif; ?>
+        <!-- Interview -->
+        <?php if ($participated_in_interview) {
+            echo("<p><span class='bold-heading'>Interview:</span>");
+             foreach ($participated_in_interview as $interview) {
+                echo("<a href='$interview[guid]'>");
+                if ($interview_year && $interview_month && $interview_day) {
+                    echo($interview_day. ' ' . $interview_month. ' ' . $interview_year. ' Interview');
+                }
+                else if($interview_month && $interview_year ) {
+                    echo ($interview_month. ' ' . $interview_year. ' Interview');
+                }
+                else {
+                    echo ($interview_year. ' Interview');
+                }
 
-        <?php if ($birth_location_village): ?>
-            <p><span class="bold-heading">Birth village:</span> <a href="<?php echo $birth_location_village[0]['guid']; ?>"><?php echo $birth_location_village['post_title']; ?></a></p>
-        <?php endif; ?>
+             }
+        }?>
 
-        <?php if ($participated_in_interview): ?>
-            <p><span class="bold-heading">Interview:</span>
-            <?php foreach ($participated_in_interview as $interview): ?>
-                <a href="<?php echo $interview['guid']; ?>">
-                    <?php if ($interview_year): ?>
-                        <?php if ($interview_month): ?>
-                            <?php if ($interview_day): ?>
-                                <?php echo $interview_day. ' ' . $interview_month. ' ' . $interview_year. ' Interview'; ?>
-                            <?php else: ?>
-                                <?php echo $interview_month. ' ' . $interview_year. ' Interview'; ?>
-                            <?php endif; ?>
-                        <?php else: ?>
-                            <?php echo $interview_year. ' Interview'; ?>
-                        <?php endif; ?>
-                    <?php endif; ?>
-                </a></p>
-            <?php endforeach; ?>
-        <?php endif; ?>
     </div>
     <div class="interviewee-image">
-        <?php if ($picture): ?>
-            <img src="<?php echo $picture; ?>" style="max-width: 320px;">
-        <?php endif; ?>
+        <?php if ($picture) {
+            echo("<img style='max-width: 320px' src='$picture[guid]'/>");
+        }?>
     </div>
 </div>
+
+
+
+
+
+
+
+
+
 
 <?php get_footer();?>
