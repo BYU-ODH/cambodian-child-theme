6109378ad9b6b3a6937efe099180a41b40014f13
Spencer Cheng <spencercheng2000@gmail.com>
2023-04-06 10:06:40 -0600

    Add Interview

--- single-interviewee.php
+++ single-interviewee.php
@@ -33,39 +33,85 @@ $story_included = $interview_pod -> field("story_included"); // $story_included[
             else if ($birth_month && $birth_year) {
                 echo("<p><span class='bold-heading'>Birth month:</span>".$birth_month . ' ' . $birth_year."</p>");
             }
-            else {
+            else if ($birth_year){
                 echo("<p><span class='bold-heading'>Birth year:</span>".$birth_year."</p>");
             }
         ?>
         <!-- Birth Location -->
         <?php if ($birth_location_province) {
-            echo("<p><span class='bold-heading'>Birth province:</span>");
+            echo("<span class='bold-heading'>Birth province:</span>");
             echo("<a href='$birth_location_province[guid]'>".$birth_location_province['post_title']."</a></p>");
         } ?>
         <!-- Birth location Village -->
         <?php if ($birth_location_village) {
-            echo("<p><span class='bold-heading'>Birth village:</span>");
+            echo("<span class='bold-heading'>Birth village:</span>");
             echo("<a href='$birth_location_village[0][guid]'>".$birth_location_village['post_title']."</a></p>");
         }?>
 
-        <!-- Interview -->
+
+        <!-- Audio info -->
+        <?php if($audio_link) { 
+            echo("<audio controls>");
+            echo("<source href='$audio_link' type='audio/mpeg'>");
+            echo("<source href='$audio_link' type='audio/x-m4a'>");
+            echo("<source href='$audio_link' type='audio/aac'>");
+            echo("</audio>");
+        } 
+        ?>
+
+        <!-- Transcript AND translation files -->
+        <div class="transcipt-translation">
+            <?php 
+            if($transcript_file) {
+                
+                echo("<a href='$transcript_file'><img src='https://cambodianoralhistoryproject.byu.edu/wp-content/uploads/2019/08/KhmerFile-1.png' style='max-width: 100px'/></a>");
+            }
+
+            if($translation_file) {
+                echo("<a href='$translation_file'>img src='https://cambodianoralhistoryproject.byu.edu/wp-content/uploads/2019/07/EnglishFile-1.png' style='max-width: 100px'/></a>");
+            
+            }
+
+            ?>
+        </div>
+
+
+
+        <h2>Interview(s)</h2>
+
+
+        <!-- Interview Date -->
         <?php if ($participated_in_interview) {
-            echo("<p><span class='bold-heading'>Interview:</span>");
+            echo("<span class='bold-heading'>Date:</span>");
              foreach ($participated_in_interview as $interview) {
-                echo("<a href='$interview[guid]'>");
                 if ($interview_year && $interview_month && $interview_day) {
                     echo($interview_day. ' ' . $interview_month. ' ' . $interview_year. ' Interview');
                 }
                 else if($interview_month && $interview_year ) {
                     echo ($interview_month. ' ' . $interview_year. ' Interview');
                 }
-                else {
+                else if($interview_year){
                     echo ($interview_year. ' Interview');
                 }
 
              }
         }?>
 
+        <!-- Interviewer -->
+
+       <?php 
+       if($interviewer){
+            echo("<p><span class='bold-heading'> Interviewer:</span></p>");
+            echo($interviewer["post_title"]);
+       }
+       ?>
+
+        <!-- Interview information -->
+        <?php if($link_to_box_folder){
+            
+            echo("<p><a href='$link_to_box_folder'>Interview Documents</a></p>");
+        }
+        ?>
     </div>
     <div class="interviewee-image">
         <?php if ($picture) {
@@ -74,8 +120,18 @@ $story_included = $interview_pod -> field("story_included"); // $story_included[
     </div>
 </div>
 
+<!-- Navigation -->
 
-
+<nav id="nav-below" class="navigation" role="navigation">
+<div class="nav-previous">
+    <em>Previous Post</em>
+    <?php previous_post_link(); ?>   
+</div>
+<div class="nav-next">
+    <em>Next Post</em>
+    <?php next_post_link(); ?>
+</div>
+</nav>
 
 
 
