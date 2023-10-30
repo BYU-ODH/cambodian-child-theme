f1954d10cdded9ea53146fd010fd3acb843659b6
Spencer Cheng <spencercheng2000@gmail.com>
2023-10-30 11:47:01 -0600

    create cards from different file

---
+++ page-interviews-with-english-translations.php
@@ -0,0 +1,52 @@
+<?php 
+get_header();
+
+function english_translation() {
+    
+    $params = array(
+        'orderby' => 't.post_title ASC',    
+        'limit' => -1,
+        'where' => 'translation_file.meta_value != ""'
+    );
+    
+    $interviewee_params = array(
+        'orderby' => 't.post_title ASC',    
+        'limit' => -1,
+    );
+
+    $mypod = pods( 'interview' , $params);
+
+    $cards = ''; // Initialize an empty string to store card HTML
+
+    while ($mypod->fetch()) {
+        $id = $mypod->field('id');
+        $permalink = get_permalink($id);
+        $interviewee = $mypod->field('interviewee');
+
+        $picture = pods('interviewee', $interviewee['ID'])->field('picture');
+        $picture_Id = !empty($picture['guid']) ? $picture['guid'] : 'https://humstaging.byu.edu/cambodianoralhistories/wp-content/uploads/2023/02/Untitled_Artwork-4-copy-3.jpg';
+        $post_title = $interviewee['post_title']; 
+
+        // Create the card HTML
+        $card = '
+            <div class="interviewee-card">
+                <a class="link" href="' . $permalink . '">
+                    <img src="' . $picture_Id . '" alt="' . $post_title . '">
+                    <h2>' . $post_title . '</h2>
+                </a>
+            </div>
+        ';
+
+        $cards .= $card; // Append the card HTML to the cards string
+    }
+
+    return $cards; // Return the generated cards HTML
+}
+add_shortcode('english', 'english_translation');
+?>
+
+<div class="interviewee-cards-container">
+    <?php echo do_shortcode('[english]'); ?>
+</div>
+
+<?php get_footer();?>
